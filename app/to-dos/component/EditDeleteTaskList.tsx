ffe675d1e9eaf38be898966bd109655c209e1740
amro22397 <amroalmutasim22@gmail.com>
2026-06-03 14:50:57 +0400

    many changes 3-june-2026 1450 - Todo Fullstack

--- app/to-dos/component/EditDeleteTaskList.tsx
+++ app/to-dos/component/EditDeleteTaskList.tsx
@@ -1,133 +1,131 @@
-'use client'
+"use client";
 
-import { Edit2, Loader2, Trash } from 'lucide-react'
-import React, { useEffect, useState } from 'react'
+import { Edit2, Loader2, Trash } from "lucide-react";
+
+import { IoMdOpen } from "react-icons/io";
+
+import React, { useEffect, useState } from "react";
 
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog"
-import { Label } from '@radix-ui/react-label'
-import { Input } from '@/components/ui/input'
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@radix-ui/react-label";
+import { Input } from "@/components/ui/input";
 // import { useSession } from 'next-auth/react'
-import { Button } from '@/components/ui/button'
-import { TaskList } from '@/app/data/Tasks'
-import axios from 'axios'
-import { toast } from "sonner"
+import { Button } from "@/components/ui/button";
+import { TaskList } from "@/app/data/Tasks";
+import axios from "axios";
+import { toast } from "sonner";
 
 import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-  } from "@/components/ui/alert-dialog"
-import { usePathname } from 'next/navigation'
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
-  
-
-const EditDeleteTaskList = ({ tasklist, tasksList, email, 
-  fetchTasksList
- }: {
-  tasklist: TaskList, tasksList: TaskList[],
-  email: string | null | undefined,
-  fetchTasksList: () => void,
+const EditDeleteTaskList = ({
+  tasklist,
+  tasksList,
+  email,
+  fetchTasksList,
+}: {
+  tasklist: TaskList;
+  tasksList: TaskList[];
+  email: string | null | undefined;
+  fetchTasksList: () => void;
 }) => {
+  // const router = useRouter();
 
-    // const router = useRouter();
+  const pathname = usePathname() as any;
 
-    const pathname = usePathname() as any;
-
-    const taskListIconSize = 17;
-  const taskListButtonClassName = "cursor-pointer active:scale-95"
+  const taskListIconSize = 17;
+  const taskListButtonClassName = "cursor-pointer active:scale-95";
 
   const [openEditDialog, setEditDialog] = useState(false);
   const [deleteDialog, setDeleteDialog] = useState(false);
   const [loading, setLoading] = useState(false);
   const [deleteLoading, setDeleteLoading] = useState(false);
 
-
   const [formData, setFormData] = useState({
-        name: tasklist.name || "",
-        userEmail: email || "",
-      });
-  
-  
-      useEffect(() => {
-        setFormData({
-            name: tasklist.name || "",
-            userEmail: email || "",
-        })
-      }, [openEditDialog]);
+    name: tasklist.name || "",
+    userEmail: email || "",
+  });
 
+  useEffect(() => {
+    setFormData({
+      name: tasklist.name || "",
+      userEmail: email || "",
+    });
+  }, [openEditDialog]);
 
   const handleEdit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     setLoading(true);
 
-    axios.put("/api/tasks-list", { id: tasklist.id, ...formData})
-    .then(() => {
+    axios
+      .put("/api/tasks-list", { id: tasklist.id, ...formData })
+      .then(() => {
         setEditDialog(false);
-        toast.success("Task List updated successfully")
-
-    })
-    // .then(() => {
-    //     window.location.reload()
-    // })
-    .then(() => {
-      fetchTasksList();
-    })
-    .catch((error) => {
-        
-        toast.error(`${error}`)
-    })
-    .finally(() => {
-        setLoading(false)
-    })
-    
-  }
-
+        toast.success("Task List updated successfully");
+      })
+      // .then(() => {
+      //     window.location.reload()
+      // })
+      .then(() => {
+        fetchTasksList();
+      })
+      .catch((error) => {
+        toast.error(`${error}`);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
 
   const handleDelete = async (e: React.MouseEvent) => {
     e.preventDefault();
 
     setDeleteLoading(true);
 
-    axios.delete(`/api/tasks-list/${tasklist.id}`)
-    .then(() => {
-      setDeleteDialog(false);
+    axios
+      .delete(`/api/tasks-list/${tasklist.id}`)
+      .then(() => {
+        setDeleteDialog(false);
         if (pathname.includes(tasklist.id)) {
           const id = tasksList[0].id;
         }
-    })
-    .then(() => {
-
-      toast.success(`Task List deleted successfully`)
-  })
-  // .then(() => {
-  //       window.location.reload()
-  //   })
-  .then(() => {
-    fetchTasksList();
-  })
-    .catch((error) => {
-        
-        toast.error(`${error}`)
-    })
-    .finally(() => {
-      setDeleteLoading(false)
-    })
-  }
+      })
+      .then(() => {
+        toast.success(`Task List deleted successfully`);
+      })
+      // .then(() => {
+      //       window.location.reload()
+      //   })
+      .then(() => {
+        fetchTasksList();
+      })
+      .catch((error) => {
+        toast.error(`${error}`);
+      })
+      .finally(() => {
+        setDeleteLoading(false);
+      });
+  };
 
   /* useEffect(() => {
         if (tasksList.length === 0) {
@@ -135,90 +133,113 @@ const EditDeleteTaskList = ({ tasklist, tasksList, email,
         }
       }, [deleteDialog]); */
 
-  console.log(formData)
+  console.log(formData);
   return (
     <div className="flex flex-row items-center">
-                  <Edit2 size={taskListIconSize} onClick={() => setEditDialog(true)}
-                  className={`text-green-700 hover:text-green-800
+
+     <Link
+     href={`/to-dos/${tasklist?.id}`}
+     target="_blank"
+     >
+      <IoMdOpen
+      size={18}
+      className="text-black hover:text-black/95
+      dark:text-white dark:hover:text-white/95"
+      />
+     </Link>
+
+      <Edit2
+        size={taskListIconSize}
+        onClick={() => setEditDialog(true)}
+        className={`text-green-700 hover:text-green-800
                   dark:text-green-200 dark:hover:text-green-300 gap
-                  ${taskListButtonClassName}`} />
-                  <Trash size={taskListIconSize} onClick={() => setDeleteDialog(true)}
-                  className={`text-red-600 hover:text-red-700
+                  ${taskListButtonClassName}`}
+      />
+      <Trash
+        size={taskListIconSize}
+        onClick={() => setDeleteDialog(true)}
+        className={`text-red-600 hover:text-red-700
                   dark:text-red-300 dark:hover:text-red-400 gap
-                  ${taskListButtonClassName}`} />
+                  ${taskListButtonClassName}`}
+      />
 
-
-
-
-                  <Dialog open={openEditDialog} onOpenChange={() => setEditDialog(!openEditDialog)}  >
-  <DialogTrigger></DialogTrigger>
-
-
-  <DialogContent>
-    <DialogHeader>
-      <DialogTitle>Edit Task List</DialogTitle>
-      <DialogDescription>
-      Edit this task list. Click save when you&apos;re done.
-      </DialogDescription>
-    </DialogHeader>
-
-    <form className="flex flex-col gap-6 mt-8"
-    onSubmit={handleEdit}>
-
-      <div className='flex flex-col gap-2'
+      <Dialog
+        open={openEditDialog}
+        onOpenChange={() => setEditDialog(!openEditDialog)}
       >
-      <Label>List Name</Label>
-      <Input id="name" type="name" placeholder='Enter the name of the task list'
-      defaultValue={formData.name} onChange={(e) => setFormData({
-        ...formData,
-        [e.target.id]: e.target.value,
-      })} />
-      </div>
+        <DialogTrigger></DialogTrigger>
 
-      <DialogFooter className="mt-11">
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Edit Task List</DialogTitle>
+            <DialogDescription>
+              Edit this task list. Click save when you&apos;re done.
+            </DialogDescription>
+          </DialogHeader>
+
+          <form className="flex flex-col gap-6 mt-8" onSubmit={handleEdit}>
+            <div className="flex flex-col gap-2">
+              <Label>List Name</Label>
+              <Input
+                id="name"
+                type="name"
+                placeholder="Enter the name of the task list"
+                defaultValue={formData.name}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    [e.target.id]: e.target.value,
+                  })
+                }
+              />
+            </div>
+
+            <DialogFooter className="mt-11">
               <Button type="submit" className="flex items-center gap-1">
                 {loading ? (
-                  <Loader2 className='animate-spin' />
+                  <Loader2 className="animate-spin" />
                 ) : (
                   <span>Save task</span>
                 )}
               </Button>
             </DialogFooter>
-    </form>
+          </form>
+        </DialogContent>
+      </Dialog>
 
+      <AlertDialog
+        open={deleteDialog}
+        onOpenChange={() => setDeleteDialog(!deleteDialog)}
+      >
+        <AlertDialogTrigger></AlertDialogTrigger>
 
-  </DialogContent>
-</Dialog>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              Are you sure you want to delete this task list?
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone, This will permanently delete your
+              task list..
+            </AlertDialogDescription>
+          </AlertDialogHeader>
 
+          <AlertDialogFooter>
+            <AlertDialogCancel onClick={() => setDeleteDialog(false)}>
+              Cancel
+            </AlertDialogCancel>
+            <AlertDialogAction onClick={handleDelete}>
+              {deleteLoading ? (
+                <Loader2 className="animate-spin" />
+              ) : (
+                "Continue"
+              )}
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  );
+};
 
-
-
-<AlertDialog open={deleteDialog} onOpenChange={() => setDeleteDialog(!deleteDialog)}>
-  <AlertDialogTrigger></AlertDialogTrigger>
-
-  <AlertDialogContent>
-    <AlertDialogHeader>
-      <AlertDialogTitle>Are you sure you want to delete this task list?</AlertDialogTitle>
-      <AlertDialogDescription>
-        This action cannot be undone, This will permanently delete your task list..
-      </AlertDialogDescription>
-    </AlertDialogHeader>
-
-    <AlertDialogFooter>
-      <AlertDialogCancel onClick={() => setDeleteDialog(false)}>Cancel</AlertDialogCancel>
-      <AlertDialogAction onClick={handleDelete}>
-        {deleteLoading ? (
-          <Loader2 className='animate-spin' />
-        ) : 
-        "Continue"
-        }
-      </AlertDialogAction>
-    </AlertDialogFooter>
-  </AlertDialogContent>
-</AlertDialog>
-
-                </div>
-  )
-}
-
-export default EditDeleteTaskList
+export default EditDeleteTaskList;
